5f843f4f16548d2f7c7cfdd73a606dad32eae5ef
nwachukwuoparah <nwachukwuoparah@gmail.com>
2024-08-18 13:37:04 +0100

    update on socket

--- server.js
+++ server.js
@@ -2,35 +2,33 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const http = require('http');
 const socketIo = require('socket.io');
-const dotenv =require('dotenv') // Load environment variables from .env file
+const dotenv = require('dotenv'); // Load environment variables from .env file
+const { json } = require("express");
 
 // Create an HTTP server with the Express app
 const server = http.createServer(app);
 
-// Initialize socket.io with the server 
+// Initialize socket.io with the server
 const io = socketIo(server);
 
 dotenv.config({ path: "./config.env" });
 
-process.on("uncaughtException", err => {
-  console.log(err.name, err.message); 
-  console.log("UNCAUGHT EXCEPTION!"); 
-  process.exit(1);  
-});  
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION!");
+  process.exit(1);
+});
 
-const DB = process.env.DATA_BASE  
-// .replace('<PASSWORD>', process.env.DATA_BASE_PASSWORD);
- 
-console.log("run",DB);  
- 
-mongoose 
-  .connect(DB, { 
+const DB = process.env.DATA_BASE;
+
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-  }) 
+  })
   .then(() => console.log('DB connection successful!'))
-  .catch(err => {
+  .catch((err) => {
     console.log("DB connection error:", err.message);
     process.exit(1);
   });
@@ -38,18 +36,21 @@ mongoose
 // Socket.IO connection
 io.on('connection', (socket) => {
   console.log('A user connected to mock server');
+  
   // Send a success message to the client upon connection
   socket.emit('connection-success', { message: 'Successfully connected to mock server' });
-
-  // Listen for 'rider' event
+ 
+  // Listen for 'rider' event from the client
   socket.on('rider', (data) => {
-    console.log('Mock rider message received:', data);
-    io.emit('rider-message', data); // Broadcast the rider message to all connected clients
+    console.log('Mock rider message received:', JSON.parse(data));
+ 
+    // Emit the 'rider-message' event to all connected clients
+    io.emit('rider-message', data);
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('User disconnected'); 
   });
 });
 
@@ -58,7 +59,7 @@ server.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on("unhandledRejection", err => {
+process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("UNHANDLED REJECTION!");
   server.close(() => {
